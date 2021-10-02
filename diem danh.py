4eaadaf172f5462f21f5bc894a60e3c6bd8758fc
NguyenPhan100802 <90489505+NguyenPhan100802@users.noreply.github.com>
2021-10-02 11:16:16 +0700

    Update diem danh.py

--- diem danh.py
+++ diem danh.py
@@ -1,9 +1,14 @@
 import pandas as pd
-
 series_ex = {"a":[1, 4, 6, 7, 8], "b":[23, 12, 56, 8, 22], "c":[87, 65, 43, 11, 34]}
+df = pd.DataFrame(dataframe_ex)
 
-series_data = pd.DataFrame(series_ex)
+print(df)
 
-print(series_data)
-print("Xuat ra cot dau cua series")
-print(series_data.iloc[:, 0])
+print('in ra cot dau cua 1 dataframe')
+print(df['a'])
+
+print('In ra hàng đầu tiên của 1 data')
+print(df.loc[0])
+
+print('In ra phan tu dau cua mot series')
+series_data = pd.Series([1,2,3,4,5])
